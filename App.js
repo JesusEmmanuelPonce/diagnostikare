e330010913124ee0d719bf8417b242c615de8ada
JesusEmmanuelPonce <j.emmanuel.pb@hotmail.com>
2022-02-17 10:48:52 -0600

    [feat] navigation added

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+import { NavigationContainer }  from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Calendar     from "./src/screens/Calendar";
+import Prediagnosis from "./src/screens/Prediagnosis";
+
+const App = () => {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="prediagnosis">
+            <Stack.Screen name="prediagnosis" component={Prediagnosis} />
+            <Stack.Screen name="calendar" component={Calendar} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/Prediagnosis.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const Prediagnosis = () => {
+    return (
+        <View>
+            <Text>Prediagnositico</Text>
+        </View>
+    )
+};
+
+export default Prediagnosis;
